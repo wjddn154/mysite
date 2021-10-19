4be035d105918b1db93b5982a4eef630e014ef1e
wjddn154 <gjwjddn154@gmail.com>
2021-10-19 12:35:36 +0900

    mysite03 #2

--- mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/MainController.java
@@ -2,15 +2,13 @@ package com.douzone.mysite.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
 	
-	@ResponseBody
 	@RequestMapping({"", "/main"})
 	public String index() {
-		return "mysite03.index() called";
+		return "main/index";
 	}
 	
 	
