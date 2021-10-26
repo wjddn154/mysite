f3b8251d5adcdaa9b62826bf2965b1fb72ca02f3
wjddn154 <gjwjddn154@gmail.com>
2021-10-26 10:25:16 +0900

    form validation #1

--- mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.douzone.mysite.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,8 +29,19 @@ public class UserController {
 	}
 	
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(UserVO vo) {
-		userService.join(vo);
+	public String join(@Valid UserVO vo, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			List<ObjectError> list = result.getAllErrors();
+			for(ObjectError error : list) {
+				System.out.println(error);
+			}
+			
+			return "user/join";
+		}
+		
+		
+//		userService.join(vo);
 		return "redirect:/user/joinsuccess";
 	}
 	
