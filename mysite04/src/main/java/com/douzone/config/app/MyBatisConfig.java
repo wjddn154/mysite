55ab9531e39705ebf06112b8bacfd25c9aedfb8b
wjddn154 <gjwjddn154@gmail.com>
2021-10-28 13:38:42 +0900

    AppConfig #2

--- mysite04/src/main/java/com/douzone/config/app/MyBatisConfig.java
+++ mysite04/src/main/java/com/douzone/config/app/MyBatisConfig.java
@@ -2,8 +2,10 @@ package com.douzone.config.app;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +21,14 @@ public class MyBatisConfig {
 		
 		return sqlSessionFactory.getObject();
 	}
+	
+	@Bean
+	public SqlSession sqlSession(SqlSessionFactory sqlSessionFactory) {
+		
+		return new SqlSessionTemplate(sqlSessionFactory);
+	}
+	
+	
 	
 }
 
--- mysite04/src/main/java/com/douzone/mysite/config/AppConfig.java
+++ mysite04/src/main/java/com/douzone/mysite/config/AppConfig.java
@@ -6,11 +6,12 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
 import com.douzone.config.app.DBConfig;
+import com.douzone.config.app.MyBatisConfig;
 
 @Configuration
 @EnableAspectJAutoProxy	//
 @ComponentScan({"com.douzone.mysite.service", "com.douzone.mysite.repository", "com.douzone.mysite.aspect"})
-@Import({DBConfig.class})
+@Import({DBConfig.class, MyBatisConfig.class})
 public class AppConfig {
 
 	
